e47e6b9560164538c972e4e3e63a2ff190b5e90d
GooPoo <132414772+GooPoo@users.noreply.github.com>
2024-10-24 13:03:01 +0800

    Refactoring of database models, making the models inherit parent classes to have shared fields for API vs Regular Web Interface.

--- app/models.py
+++ app/models.py
@@ -2,6 +2,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from secrets import token_urlsafe
 from app import db
+from sqlalchemy.ext.declarative import declared_attr
 
 Column, Integer, String, Boolean, Date, ForeignKey, Model, relationship = (
     db.Column, db.Integer, db.String, db.Boolean, db.Date, db.ForeignKey, db.Model, db.relationship
@@ -30,6 +31,10 @@ class User(UserMixin, Model):
         db.session.commit()
 
     games = relationship('Game', order_by='Game.id', back_populates='user')
+    api_games = relationship('GameAPI', order_by='GameAPI.id', back_populates='user')
+
+
+
 
 
 class Wordlewords(Model):
@@ -43,13 +48,22 @@ class Gamewordofday(Model):
     date = Column(Date, nullable=False, unique=True)
     
     games = relationship('Game', back_populates='word_of_the_day')
+    api_games = relationship('GameAPI', back_populates='word_of_the_day')
 
 
-class Game(Model):
+
+
+
+class BaseGame(Model):
+    __abstract__ = True
     id = Column(Integer, primary_key=True)
     user_id = Column(Integer, ForeignKey('user.id'), nullable=False)
     game_word_id = Column(Integer, ForeignKey('gamewordofday.id'), nullable=False)
     complete = Column(Boolean, default=False, nullable=False)
+
+
+class Game(BaseGame):
+    __tablename__ = 'game'
     
     user = relationship('User', back_populates='games')
     word_of_the_day = relationship('Gamewordofday', back_populates='games')
@@ -57,23 +71,74 @@ class Game(Model):
     game_score = relationship('GameScore', uselist=False, back_populates='game', cascade='all, delete-orphan')
 
 
-class GameScore(Model):
-    id = Column(Integer, primary_key=True)
-    game_id = Column(Integer, ForeignKey('game.id'), nullable=False)
-    score = Column(Integer, nullable=False)
+class GameAPI(BaseGame):
+    __tablename__ = 'game_api'
     
+    user = relationship('User', back_populates='api_games')
+    word_of_the_day = relationship('Gamewordofday', back_populates='api_games')
+    guesses = relationship('GuessAPI', back_populates='game', cascade='all, delete-orphan')
+    game_score = relationship('GameScoreAPI', uselist=False, back_populates='game', cascade='all, delete-orphan')
+
+
+
+
+
+class BaseGameScore(Model):
+    __abstract__ = True
+    id = Column(Integer, primary_key=True)
+    game_id = Column(Integer, nullable=False)
+    score = Column(Integer, nullable=False)
+
+
+class GameScore(BaseGameScore):
+    __tablename__ = 'game_score'
+
+    game_id = Column(Integer, ForeignKey('game.id'), nullable=False)
+
     game = relationship('Game', back_populates='game_score')
 
 
-class Guess(Model):
+class GameScoreAPI(BaseGameScore):
+    __tablename__ = 'game_score_api'
+
+    game_id = Column(Integer, ForeignKey('game_api.id'), nullable=False)
+
+    game = relationship('GameAPI', back_populates='game_score')
+
+
+
+
+
+class BaseGuess(Model):
+    __abstract__ = True
     id = Column(Integer, primary_key=True)
-    game_id = Column(Integer, ForeignKey('game.id'), nullable=False)
+    game_id = Column(Integer, nullable=False)
     guess_number = Column(Integer, nullable=False)
     guess_word = Column(String(5), nullable=False)
     guess_score = Column(String(5), nullable=False)
-    
+
+
+class Guess(BaseGuess):
+    __tablename__ = 'guess'
+
+    game_id = Column(Integer, ForeignKey('game.id'), nullable=False)
+
     game = relationship('Game', back_populates='guesses')
 
-# Indexes for Guess table for fast lookup
+
+class GuessAPI(BaseGuess):
+    __tablename__ = 'guess_api'
+
+    game_id = Column(Integer, ForeignKey('game_api.id'), nullable=False)
+
+    game = relationship('GameAPI', back_populates='guesses')
+
+
+
+
+
 db.Index('idx_game_id', Guess.game_id)
 db.Index('idx_game_id_guess_number', Guess.game_id, Guess.guess_number)
+
+db.Index('idx_apigame_id', GuessAPI.game_id)
+db.Index('idx_apigame_id_guess_number', GuessAPI.game_id, GuessAPI.guess_number)
