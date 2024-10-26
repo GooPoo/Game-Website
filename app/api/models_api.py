0f1acd8e89d70b578751fbfedb89944f65e8bc54
GooPoo <132414772+GooPoo@users.noreply.github.com>
2024-10-26 16:12:27 +0800

    Logic errors in calculating scores and validation of words fixed for API endpoints, Submitting of word no longer is allowed for games created on previous days.

--- app/api/models_api.py
+++ app/api/models_api.py
@@ -6,7 +6,52 @@ from sqlalchemy.exc import NoResultFound
 from app.models import GameAPI, GuessAPI, GameScoreAPI, Gamewordofday, Wordlewords
 from app import db, csrf
 from app.api import api_bp
-from app.routes import validate_word
+
+def validate_word(word, game_id):
+
+    valid_word = Wordlewords.query.filter_by(word=word).first()
+    if not valid_word:
+        return "bad"
+    
+    game = GameAPI.query.filter_by(id=game_id).first()
+    target_word = game.word_of_the_day.word
+
+    feedback = [0] * 5 
+    target_word_letters = list(target_word)
+
+    for i in range(5):
+        if word[i] == target_word[i]:
+            feedback[i] = 2
+            target_word_letters[i] = None 
+
+    for i in range(5):
+        if feedback[i] != 2 and word[i] in target_word_letters:
+            feedback[i] = 1
+            target_word_letters[target_word_letters.index(word[i])] = None 
+
+    return ''.join(map(str, feedback))
+
+
+
+def calculate_game_score(game_id):
+    guesses = GuessAPI.query.filter_by(game_id=game_id).all()
+
+    total_feedback_score = 0
+    won_game = False
+
+    for guess in guesses:
+        score_str = guess.guess_score
+        total_feedback_score += sum(int(char) for char in score_str)
+        if score_str == '22222':
+            won_game = True
+
+    guess_count = len(guesses)
+
+    if won_game:
+        final_score = total_feedback_score * (2 ** (7 - guess_count))
+    else:
+        final_score = total_feedback_score
+    return final_score
 
 
 # List all games that the user has participated in, with scores and status
@@ -68,14 +113,43 @@ def create_game_api(user):
 def submit_word_api(user, gameid):
     data = request.get_json()
     word = data.get('word')
+    current_date = date.today()
 
     if not word:
         return jsonify({'error': 'Missing word'}), 400
+
+    gamewordofday = Gamewordofday.query.filter_by(date=current_date).one_or_none()
+
+    if not gamewordofday:
+        return jsonify({'error': 'Fetching word of the day'}), 400
+
+    game = GameAPI.query.filter_by(id=gameid, game_word_id=gamewordofday.id).first()
+
+    if not game:
+        return jsonify({'error': 'Game is not playable'}), 400
     
     feedback = validate_word(word, gameid)
 
     if feedback == "bad":
         return jsonify({'error': 'Invalid word'}), 400
+    
+    new_guess = GuessAPI(
+        game_id=gameid,
+        guess_number=len(game.guesses) + 1,
+        guess_word=word,
+        guess_score=feedback
+    )
+    db.session.add(new_guess)
+
+    guess_count = len(game.guesses)
+    if feedback == '22222' or guess_count + 1 >= 6:
+        game.complete = True
+
+        score = calculate_game_score(gameid)
+        game_score = GameScoreAPI(game_id=gameid, score=score)
+        db.session.add(game_score)
+
+    db.session.commit()
 
     return jsonify({'feedback': feedback})
 
