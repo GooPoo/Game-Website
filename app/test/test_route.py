b1261e8d33cc315f85ae68fec60d56a8479546a8
GooPoo <132414772+GooPoo@users.noreply.github.com>
2024-10-29 14:08:51 +0800

    Errors with database2

---
+++ app/test/test_route.py
@@ -0,0 +1,14 @@
+from flask import Blueprint, jsonify
+from app import db
+from app.models import User
+
+test_bp = Blueprint('test', __name__)
+
+@test_bp.route('/test_query')
+def test_query():
+    try:
+        # Attempt to query all users
+        users = User.query.all()
+        return jsonify({"message": "Query successful!", "users": [user.username for user in users]}), 200
+    except Exception as e:
+        return jsonify({"message": "Query failed!", "error": str(e)}), 500
